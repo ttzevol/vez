82197fc3fdafbf38c257097d884c31e094471334
wubowen <834872527@qq.com>
2021-01-22 17:31:08 +0800

    一些小的改进

--- src/main/java/com/vez/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vez/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.vez.exception.handler;
+package com.vez.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
